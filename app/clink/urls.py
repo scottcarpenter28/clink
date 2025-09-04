6170377d045a9ede8421a7fc7b338229cc352a5b
Scott Carpenter <scottcarpenter28@gmail.com>
2025-09-04 04:42:55 -0400

    Fix redirect loop

--- app/clink/urls.py
+++ app/clink/urls.py
@@ -16,13 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-
-def root_redirect(request):
-    return redirect('finance:login')
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', root_redirect, name='root'),
     path('', include('finance.urls')),
 ]
